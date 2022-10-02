11e7ccb2400d78db413ad8b1970494832185fb40
feroz <feroz@example.com>
2022-10-02 08:00:44 +0530

    second commit

--- automation.sh
+++ automation.sh
@@ -22,7 +22,6 @@ if ! pidof apache2 > /dev/null
 then
     echo "apache2 is not running !! restarting"
     systemctl restart apache2 
-    sleep 10
 else
 	echo "apache2 is running"
 	service apache2 status |grep running
@@ -38,8 +37,8 @@ else
 fi
 
 echo "==================================> making tar of log files <================================"
-cd /var/log/apache2 && tar -czf $myname-$logtype-${timestamp}.$type *.log 
-mv /var/log/apache2/$myname-$logtype-${timestamp}.$type /tmp/
+cd /var/log/apache2 && tar -czf $myname-$logtype-${timestamp}.$type *.log && 
+mv $myname-$logtype-${timestamp}.$type /tmp/
 echo "done collecting logs"
 
 
@@ -47,4 +46,30 @@ echo "done collecting logs"
 echo "===================================> copying to s3 <========================================="
 aws s3 	cp /tmp/$myname-$logtype-${timestamp}.$type s3://$s3_bucket/$myname-$logtype-${timestamp}.$type
 
+
+echo "==================================> inventory.html <========================================="
+path="/var/www/html"
+if [[  -s ${path}/inventory.html ]]
+then
+	echo "file found"
+else 
+	echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${path}/inventory.html
+fi
+
+size=$(du -h /tmp/$myname-httpd-logs-${timestamp}.tar | awk '{print $1}')
+if [[ -f ${path}/inventory.html ]] 
+then
+	echo -e "$logtype\t-\t${timestamp}\t-\t$type\t-\t${size}" >> ${path}/inventory.html
+fi
+
+echo "======================================> setting cronjob <======================================"
+if [ -s "/etc/cron.d/automation" ]
+then
+echo "File is found"
+
+else
+  cat >>/etc/cron.d/automation << EOF
+   0 0 * * *  /root/Automation_project/automation.sh
+EOF
+fi
 
