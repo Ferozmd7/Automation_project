07269cce6b80d6e1b77445ddc44ce7180e7bbab6
feroz <feroz@example.com>
2022-10-02 07:22:49 +0530

    intial commit

---
+++ automation.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+s3_bucket='upgrad-feroz'
+myname='feroz'
+timestamp=$(date +"%d%m%Y-%H%M%S")
+logtype='httpd-logs'
+type='tar'
+
+
+echo "===============================> Update <==================================================="
+sudo apt update -y
+
+echo "===============================> installing & verifying apache2 <============================"
+if which apache2 > /dev/null ; then
+	echo "already installed : $(dpkg-query -W -f='${Version}\n' apache2)"
+else
+   echo "Installing"
+sudo apt install apache2 -y
+fi
+
+echo "===============================> checking running or not <==================================="
+if ! pidof apache2 > /dev/null
+then
+    echo "apache2 is not running !! restarting"
+    systemctl restart apache2 
+    sleep 10
+else
+	echo "apache2 is running"
+	service apache2 status |grep running
+
+fi
+
+echo "===============================> enable checking <==========================================="
+if [[ $(systemctl list-unit-files | grep apache2.service | awk '{ print $2}') ==  "enabled" ]] ; then
+	echo "enabled"
+else 
+	echo "enabling"
+	systemctl enable apache2 
+fi
+
+echo "==================================> making tar of log files <================================"
+cd /var/log/apache2 && tar -czf $myname-$logtype-${timestamp}.$type *.log 
+mv /var/log/apache2/$myname-$logtype-${timestamp}.$type /tmp/
+echo "done collecting logs"
+
+
+
+echo "===================================> copying to s3 <========================================="
+aws s3 	cp /tmp/$myname-$logtype-${timestamp}.$type s3://$s3_bucket/$myname-$logtype-${timestamp}.$type
+
+
